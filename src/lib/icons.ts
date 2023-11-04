a5d7377e0837437772d4309c30754eb737539c62
aberigle <aberigle@uhurus.com>
2023-11-04 13:48:09 +0100

    add coffe icon

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -2,6 +2,7 @@ import { IconDefinition, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 interface SocialIcons {
   [key: string] : IconDefinition
@@ -11,7 +12,8 @@ const icons: SocialIcons = {
   "twitter"  : faTwitter,
   "github"   : faGithub,
   "linkedin" : faLinkedin,
-  "mail"     : faEnvelope
+  "mail"     : faEnvelope,
+  "coffee"   : faCoffee
 }
 
 export function getSVGIcon(icon: string) : string {
