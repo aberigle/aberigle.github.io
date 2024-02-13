f7e1e437aae3ac081c3992a3a4e909d801ddb8c2
aberigle <aberigle@uhurus.com>
2024-02-13 14:55:43 +0100

    add nested rollup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 import pugPlugin from "vite-plugin-pug";
 
 export default defineConfig({
-  plugins: [pugPlugin({ pretty: true })]
+  plugins: [pugPlugin({ pretty: true })],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'notion/index.html'),
+      },
+    }
+  }
+
 })
