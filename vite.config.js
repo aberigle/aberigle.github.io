5ee559aabcfa1fafbc0d94c54854ec06ebc506dc
aberigle <aberigle@uhurus.com>
2024-02-13 15:07:15 +0100

    fix build

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'notion/index.html'),
+        nested: resolve(__dirname, 'cheap-notion/index.html'),
       },
     }
   }
